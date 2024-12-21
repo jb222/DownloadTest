31dd85bc2b657b179d1a69160aed1472930e9551
jb222 <61352184+jb222@users.noreply.github.com>
2024-12-21 17:24:51 +0100

    Create downloadtest.user.js

---
+++ downloadtest.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         UpdateViaGitHub
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include      *://www.iex.tld/*
+// @updateURL    
+// @downloadURL  
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("Script:", GM_info.script.name);
+    console.log("Versie:", GM_info.script.version);
+        // Your code here...
+})();
