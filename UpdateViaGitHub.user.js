1bbfcf7e25d45991696da632f6ca1558db9a915c
jb222 <61352184+jb222@users.noreply.github.com>
2024-12-22 13:19:58 +0100

    Update UpdateViaGitHub.user.js

--- UpdateViaGitHub.user.js
+++ UpdateViaGitHub.user.js
@@ -5,8 +5,8 @@
 // @description  try to take over the world!
 // @author       You
 // @include      *://www.iex.tld/*
-// @updateURL    https://github.com/jb222/DownloadTest/raw/ca7dc152397d8b3a28baacccd701b19a341958ba/UpdateViaGitHub.user.js
-// @downloadURL  
+// @updateURL    https://github.com/jb222/DownloadTest/raw/main/UpdateViaGitHub.user.js
+// @downloadURL  https://github.com/jb222/DownloadTest/raw/main/UpdateViaGitHub.user.js
 // @grant        none
 // ==/UserScript==
 
